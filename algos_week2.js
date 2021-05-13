87b657267c7c2621bf59224b05a3bf815a72f039
Junny Lee <junny1997@gmail.com>
2021-05-13 13:04:47 -0700

    another day of algos added

--- algos_week2.js
+++ algos_week2.js
@@ -6,6 +6,8 @@
     Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
 */
 
+import { object } from "prop-types";
+
 // cody's solution
 function orderedIntersection(sortedA, sortedB){
     // iterating through the two arrays
@@ -336,3 +338,100 @@ function symmetricDifference(setA, setB){
 let setA = [1,2,3,4];
 let setB = [1,2,3,5,5,2];
 console.log(symmetricDifference(setA, setB))
+
+
+// ****************************************************s
+// Thursday
+/* 
+    Given an array of ints, find all the non-consecutive integers
+    A number is non-consecutive if it is NOT exactly 1 larger than the previous element.
+    The first element is never considered non-consecutive.
+    Return an array of objects where each object contains the number itself
+    and it's index.
+*/
+function allNonConsecutive(sortedNums){
+    let returnArr = [];
+    let objectToAdd = {};
+    
+    for (let j = 1; j < sortedNums.length; j++){
+        if (sortedNums[j] != sortedNums[j-1] + 1){
+            objectToAdd = {
+                i : j,
+                n : sortedNums[j]
+            };
+            // objectToAdd.i = j;// doesnt work because these are references 
+            // objectToAdd.n = sortedNums[j];
+            returnArr.push(objectToAdd);
+        }
+    }
+    return returnArr;
+}
+
+let sortedNums = [1,2,3,4,6,7,8,10];
+let expected = 
+    [
+        { i: 4, n: 6},
+        { i: 7, n: 10 }
+    ]
+// because at index 4, the value is 6, which is not the previous value plus 1 (4 + 1 = 5, not 6), 
+// and at index 7, the value is 10, which is not the previous value plus 1 (8 + 1 = 9, not 10)
+console.log(allNonConsecutive(sortedNums));
+
+/* 
+    You are given a list of integers. Find all the consecutive sets of 
+    integers that adds up to the sum passed in as one of the inputs.
+*/
+function findConsqSums(nums, targetSum){
+    let returnArr = [];
+    let arrToAdd = [];
+    let sum = 0;
+    let j = 0;
+
+    for (let i = 0; i < nums.length; i++){
+        while (sum != targetSum && sum < targetSum){
+            sum += nums[i+j];
+            j++;
+        }
+        if (sum == targetSum){
+            for (let k = 0; k < j; k++){
+                arrToAdd[k] = nums[i+k];
+            }
+            returnArr.push(arrToAdd);
+            arrToAdd = [];
+        }
+        sum = 0;
+        j=0;
+    }
+    return returnArr
+}
+
+// reid's solution:
+function findConsqSums(nums, t){
+    let ret = [];
+    for (let x = 0; x < nums.length; x++){
+        let sum = 0;
+        let tempArr = [];
+        let y = x;
+        while (y < nums.length && sum < t){
+            sum += nums[y];
+            tempArr.push(nums[y]);
+            if (sum === t){
+                ret.push(tempArr);
+                break;
+            }
+            y++;
+        }
+    }
+    return ret;
+}
+
+let nums = [2, 5, 3, 6, 7, 23, 12] ;
+let targetSum = 16;
+let expected = 
+[
+    [2, 5, 3, 6],
+    [3, 6, 7]
+];
+// because 2 + 5 + 3 + 6 = 16, and those numbers are all consecutive, and 3 + 6 + 7 = 16, 
+// and those numbers are also all consecutive
+console.log(findConsqSums(nums, targetSum))
