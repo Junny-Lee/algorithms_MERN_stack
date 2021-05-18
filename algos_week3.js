f7eebe2483f52e4f6d5ad8b32da8afcb0bac9bc6
Junny Lee <junny1997@gmail.com>
2021-05-18 10:03:42 -0700

    another day of algos

--- algos_week3.js
+++ algos_week3.js
@@ -158,3 +158,118 @@ console.log(insertFunctional(table, insertData))
 
 // ******************************************************************
 // Tuesday
+/* 
+    Given a search criteria object whose values will only be
+    primitives (ints, strings, booleans) and a list of objects.
+    return any object that matches all the key value pairs in the search
+    criteria object.
+    Bonus: write a 2nd solution using build in methods to practice functional
+    programming.
+    // Bonus 2: write this as an array prototype method
+    Array.prototype.methodName = function(someparameter){
+
+    }
+*/
+
+// Trey's solution 
+function findObjects(criteria, collection){
+    let output = [];
+    let flag = false;
+    for (let i = 0; i < collection.length; i++){
+        for (key in criteria){
+            if (criteria[key] == collection[i][key]) flag = true;
+            else {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            output.push(collection[i])
+        }
+    }
+    return output
+}
+
+const collection1 = [
+    { firstName: "Bob", lastName: "Bobbert", age: 31 }, { firstName: "John", lastName: "Smith", age: 25 }, { firstName: "Bob", lastName: "Smith", age: 27 },{ firstName: "Bob", lastName: "White", age: 31 },
+];
+
+const criteria1 = {firstName: "Bob", age: 31,};
+const expected1 = [
+    { firstName: "Bob", lastName: "Bobbert", age: 31 },
+    { firstName: "Bob", lastName: "White", age: 31 },
+];
+console.log(findObjects(criteria1, collection1))
+
+
+
+function findObjectsFunctional = (criteria, collection) => collection.filter(item => Object.keys(criteria).every(key => item[key] === criteria[key]))
+
+// BONUS 2:
+Array.prototype.find = function(criteria){
+    return this.filter(item => Object.keys(criteria).every(key => item[key] === criteria[key]))
+}
+
+/* 
+    Given an id, an object that has keys with corresponding updated values, and an array of objects
+    Find the object by "id" key that matches the given id value and then update that object's
+    keys with the provided new values.
+    Return the updated object, or null if no object was found
+    
+    BONUS: accept a 4th parameter that is a function to be run on the update
+*/
+const students = [
+    {
+        id: 1,
+        name: "student1",
+        isLateToday: false,
+        lateCount: 15,
+        redBeltStatus: false,
+    },
+    {
+        id: 2,
+        name: "student2",
+        isLateToday: false,
+        lateCount: 1,
+        redBeltStatus: false,
+    },
+    {
+        id: 3,
+        name: "student3",
+        isLateToday: false,
+        lateCount: 0,
+        redBeltStatus: false,
+    },
+];
+const searchId = 3;
+const updateData = { redBeltStatus: true, isLateToday: true };
+const expected2 = {
+    id: 3,
+    name: "student3",
+    isLateToday: true,
+    lateCount: 0,
+    redBeltStatus: true,
+};
+console.log(findByIdAndUpdate(searchId, updateData, students))
+
+function findByIdAndUpdate(id, updatedVals, collection) {
+    let found = false;
+    let index = 0;
+
+    while (!found){
+        for (key in collection){
+            if (collection[id] == id){
+                found = true;
+                for (key in updatedVals){
+                    collection[key] = updatedVals[key];
+                }
+                return collection[index]
+            }
+            index++;
+        }
+    }
+    return {}
+}
+
+// ******************************************************************
+// Wednesday
